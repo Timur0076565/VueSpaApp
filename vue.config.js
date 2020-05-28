ad93a8d957f5dc42bcdee1b27ac8a43086fda5c1
Timur <timurcik65@ukr.net>
2020-05-29 02:52:27 +0300

    Initial dist subtree commit

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/VueSpaApp/'
-      : '/'
-  }
+// module.exports = {
+//     publicPath: process.env.NODE_ENV === 'production'
+//       ? '/VueSpaApp/'
+//       : '/'
+//   }
 
-  // module.exports = { baseUrl: '/VueSpaApp/' }
+module.exports = { publicPath: '/VueSpaApp/' }
